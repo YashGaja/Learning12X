ed2df402b074c0b370a52ce735073f66654a8612
DELL <yashgajabhiye6@gmail.com>
2025-05-31 00:43:06 +0530

    Functions

---
+++ src/Ex_13_Functions/Lab125_Functions.java
@@ -0,0 +1,13 @@
+package Ex_13_Functions;
+
+public class Lab125_Functions {
+    public static void main(String[] args) {
+
+        // Built - in
+        // Created by the java Guys.
+        int result = Math.max(3,4);  //will print min or max number as a result
+        System.out.println(result);
+
+
+    }
+}
---
+++ src/Ex_13_Functions/Lab130_Interview_Q.java
@@ -0,0 +1,23 @@
+package Ex_13_Functions;
+
+public class Lab130_Interview_Q {
+    public static void main(String[] args) {
+
+    }
+
+    public static void main(String args) {
+
+    }
+
+    public static int main(int args) {
+        return 10;
+    }
+
+    public static int main(float args) {
+        return 10;
+    }
+
+    public static boolean main(boolean args) {
+        return true;
+    }
+}
